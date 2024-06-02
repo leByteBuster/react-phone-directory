c05b2ea9c538f73229edb6c677f06cef2401bee8
Axel Schuster <axel_schuster_@gmx.net>
2024-06-02 14:25:29 +0200

    Add components for a search bar and for a result list
    These are only the templates to create a search bar and a
    result list.
    The search bar is supposed to consist of an input field
    to enter search queries. The result list is supposed to
    show live results of the search.

---
+++ src/components/ResultList/ResultList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './result-list.scss'
+
+
+export default function ResultList({ results }) {
+  return (
+  <Container>
+  </Container>
+  );
+};
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './search-bar.scss'
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired
+};
+
+export default function SearchBar({ onSearch }) {
+  return (
+    <Box>
+    </Box>
+      );
+};
