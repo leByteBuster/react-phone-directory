a624ce683ac42cae720e2c5af7a11c294927613d
Axel Schuster <axel_schuster_@gmx.net>
2024-06-02 15:31:58 +0200

    Add fetchService and handler which triggers a fetch on search
    A fetch service is added to fetch data. A mockFetch function is
    added to simulate fetching data from a server. In the main component
    Add.js a handler is added which triggers the fetching based upon the
    input in the search bar. Is passed to the search bar component.
    Furthermore, the telefonuch.json is added to src/assets in case
    i want to use it for testing purposes later.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import React, { useState } from 'react';
 import ResultsList from './components/ResultList/ResultList.js'; // TODO: same as below 
 import SearchBar from './components/SearchBar/SearchBar.js'; // TODO: somtimes i have to specify SearchBar.js directly. why ??
+import fetchService from './services/fetchService';
 import Box from '@mui/material/Box';
 
 function App() {
   const [results, setResults] = useState([]);
+
+  const handleSearch = (query) => {
+    console.log("Query", query)
+    if (!query){
+      setResults([]);
+      return;
+    }
+    try {
+      // TODO: start apiService with capital letter ? 
+      fetchService.mockFetch(query).then((tmpRes) => tmpRes.json()).then((result => {
+        console.log(result);
+        setResults(result);}
+      ));
+    } catch (error) {
+      console.log('Error fetching data:', error)
+      setResults([]);
+    }
+  };
+
   return (
     <>
       <Box className='App' 
---
+++ src/services/fetchService.js
@@ -0,0 +1,67 @@
+const API_BASE_URL = 'tobedone';
+
+const fetchService = {
+
+  // basic fetch function. TODO: pass typed in letters to filter result server side
+  fetchData: async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/endpoint`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle error appropriately
+      throw error;
+    }
+  },
+
+  mockFetch: (query) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const data = [
+              { "id": 1, "name": "Harper-Rose Lennon", "phone": "0171/24984023" },
+              { "id": 2, "name": "Lloyd Mitchell", "phone": "0171/14294053" },
+              { "id": 3, "name": "Aminah Sheldon", "phone": "0176/33581541" },
+              { "id": 4, "name": "Skylah Gibbons", "phone": "0176/30053109" },
+              { "id": 5, "name": "Owen Wilkes", "phone": "0176/52364266" },
+              { "id": 6, "name": "Kajol Carty", "phone": "0176/23200162" },
+              { "id": 7, "name": "Mohammod Finch", "phone": "0171/30608407" },
+              { "id": 8, "name": "Khaleesi Randolph", "phone": "0171/75813522" },
+              { "id": 9, "name": "Bryan Rankin", "phone": "0171/38068474" },
+              { "id": 10, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 11, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 12, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 13, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 14, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 15, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 16, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 17, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 18, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 19, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 20, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 21, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 22, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 23, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 24, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 25, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 26, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 27, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 28, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 29, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 30, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 31, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 32, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 33, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 34, "name": "Luke Mason", "phone": "0176/47664269" },
+              { "id": 35, "name": "Luke Mason", "phone": "0176/47664269" },
+            ];
+            var filtered = data.filter(entry => entry.name.includes(query))
+            resolve({ json: () => Promise.resolve(filtered) });
+        }, 100); // simulate network delay
+    });
+}
+};
+
+export default fetchService;
