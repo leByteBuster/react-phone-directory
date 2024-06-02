0f99701cc9f86a164b767824e842e9909a8d3d17
Axel Schuster <axel_schuster_@gmx.net>
2024-06-02 14:49:13 +0200

    Add mui text field for search bar. Add search bar to parent component.
    Adds text field functionality with mui components. Also applies some
    styling for the search bar. the search bar is added to the parent
    component App.js.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar.js'; // TODO: somtimes i have to specify SearchBar.js directly. why ??
+import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box className='App' 
+        <SearchBar onSearch={handleSearch}/>
+      </Box>
+    </>
   );
 }
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,13 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './search-bar.scss'
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 SearchBar.propTypes = {
   onSearch: PropTypes.func.isRequired
 };
 
 export default function SearchBar({ onSearch }) {
   return (
-    <Box>
+    <Box 
+      component="form" 
+      noValidate 
+      position="relative"
+      autoComplete="off"
+      sx={{
+        backgroundColor: 'oldlace',
+        width: 400,
+    }}    
+    >
+      <TextField
+        fullWidth
+        size="small"
+        className='search-field'
+        variant="filled"
+        //label="search"
+        placeholder="Search..."
+        InputProps={
+          { 
+          disableUnderline: true,
+          style: { textAlign: 'center'},
+            sx: { 
+                fontSize: 16, 
+                disableUnderline: true,
+                paddingLeft: 1,
+                paddingBottom: 1.8,
+                height: 42,
+              }
+          }
+        }
+        onChange={(e) => onSearch(e.target.value)}
+      >
+      </TextField>
     </Box>
       );
 };
