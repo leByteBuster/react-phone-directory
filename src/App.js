9b6bcdd087792c25ea61ae89961717252e6bceea
Axel Schuster <axel_schuster_@gmx.net>
2024-06-02 15:01:32 +0200

    Add list to show the entries of a result list
    This list is supposed to show the entries of the query
    which is triggered by the user input into the search bar.
    It uses a data grid component from mui. I removed header
    and footer and allowed only one page of pagination for
    styling causes. The list is added to the parent Add.js
    below the search bar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import ResultsList from './components/ResultList/ResultList.js'; // TODO: same as below 
 import SearchBar from './components/SearchBar/SearchBar.js'; // TODO: somtimes i have to specify SearchBar.js directly. why ??
 import Box from '@mui/material/Box';
 
 function App() {
+  const [results, setResults] = useState([]);
   return (
     <>
       <Box className='App' 
-        <SearchBar onSearch={handleSearch}/>
+        <SearchBar onSearch={handleSearch} listLength={results.length} />
+        <ResultsList results={results} />
       </Box>
     </>
   );
--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -2,10 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './result-list.scss'
 
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { DataGrid} from '@mui/x-data-grid';
+
+ResultList.propTypes = {
+  results: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      phone: PropTypes.string
+    })
+  ).isRequired
+};
+
+const columns = [
+  { field: 'name', headerName: 'Name', width: 180 },
+  { field: 'phone', headerName: 'Phone Number', width: 180 },
+];
+
 
 export default function ResultList({ results }) {
   return (
-  <Container>
+  <Container sx={{ width: 450, maxHeight: '80vh', visibility: results.length > 0 ? 'visible' : 'hidden' }}>
+    <Box sx={{ height: '100%', width: '100%'}}>
+      <DataGrid
+        slots={{
+          columnHeaders: () => null,
+        }}
+        rows={results}
+        columns={columns}
+        pageSize={1}
+        rowsPerPageOptions={[1]}
+        localeText={{noRowsLabel:""}}
+        hideFooter={true}
+        sx={{
+            height: '100%',
+            maxHeight: '100%',
+            '& .MuiDataGrid-virtualScroller': {
+              maxHeight: '100%',
+            },
+          }}
+      />
+    </Box>
   </Container>
   );
 };
