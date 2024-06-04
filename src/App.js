9e2b4d7151b0226f48e46e0674ac407a3b624af2
Axel Schuster <axel_schuster_@gmx.net>
2024-06-04 17:01:20 +0200

    Add buttons to change theme, adjust themes
    Clean and change the themes. 4 Themes are now
    available. dark, light, rose and orange. Furthermore,
    4 buttons are added to switch between the theems.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,78 @@
 import './App.css';
 import React, { useState, useMemo } from 'react';
-import ResultsList from './components/ResultList/ResultList.js'; // TODO: same as below 
-import SearchBar from './components/SearchBar/SearchBar.js'; // TODO: somtimes i have to specify SearchBar.js directly. why ??
+import CircularButton from './components/CircularButton/CircularButton';
+import ResultsList from './components/ResultList/ResultList'; // TODO: same as below 
+import SearchBar from './components/SearchBar/SearchBar'; // TODO: somtimes i have to specify SearchBar.js directly. why ??
 import fetchService from './services/fetchService';
 
 import { createTheme } from '@mui/material/styles';
-import { ThemeProvider, CssBaseline, Box } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider} from '@mui/material';
 
 // TODO: place this outside when working
-const lightTheme = {
+
+const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#1976d2', 
-    },
-    secondary: {
-      main: '#dc004e',
+      main: '#f0ead3', 
     },
     background: {
-      default: '#ffffff',
-      paper: '#f5f5f5',
+      //default: '#ffffff',
+      //paper: '#f5f5f5',
     },
   },
-};
+ });
 
-const darkTheme = {
+const orangeTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFA500', 
+    },
+  },
+});
+
+const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     primary: {
       main: '#000000',
     },
-    secondary: {
-      main: '#000000',
-    },
-    background: {
-      default: '#121212',
-      //paper: '#424242',
-    },
   },
-};
+});
 
-const thirdTheme = {
+const roseTheme = createTheme({
   palette: {
     primary: {
       main: '#E1AFD1',
     },
-    secondary: {
-      main: '#7469B6',
-    },
-    background: {
-      default: '#ffffff',
-      paper: '#424242',
-    },
   },
-};
+});
 
 export default function App() {
+
   const [results, setResults] = useState([]);
-  const [theme, setTheme] = useState('third');
+  const [theme, setTheme] = useState('rose');
+
+  // we need these so the buttons can be styled in their theme color permanently
+  const lightThemePrimaryColor = lightTheme.palette.primary.main;
+  const roseThemePrimaryColor = roseTheme.palette.primary.main;
+  const orangeThemePrimaryColor = orangeTheme.palette.primary.main;
+  const darkThemePrimaryColor = darkTheme.palette.primary.main;
+
+  const currentTheme = useMemo(() => {
+    if (theme === 'light') return lightTheme;
+    if (theme === 'dark') return darkTheme;
+    if (theme === 'orange') return orangeTheme;
+    return roseTheme;
+  }, [theme]);
+
+  const handleClick = (theme) => {
+    toggleTheme(theme)    
+  };
+
+  const toggleTheme = (newTheme) => {
+    setTheme(newTheme);
+  };
 
   const handleSearch = (query) => {
     console.log("Query", query)
@@ -77,16 +92,6 @@ export default function App() {
     }
   };
 
-  const currentTheme = useMemo(() => {
-    if (theme === 'light') return createTheme(lightTheme);
-    if (theme === 'dark') return createTheme(darkTheme);
-    return createTheme(thirdTheme);
-  }, [theme]);
-
-  const toggleTheme = (newTheme) => {
-    setTheme(newTheme);
-  };
-
   return (
     <ThemeProvider  theme={currentTheme}>
       <CssBaseline enableColorScheme/>
@@ -99,6 +104,50 @@ export default function App() {
         }}>
         <SearchBar onSearch={handleSearch} listLength={results.length} />
         <ResultsList results={results} />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'end',
+            alignItems: 'center',
+            position: 'fixed',
+            gap: '0.8em',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            height: '4em', 
+            paddingRight: '3em',
+            paddingBottom: '1em',
+            //background-color: none;
+          }}>
+          <CircularButton
+            onClick={handleClick}
+            theme="light"
+            color={lightThemePrimaryColor}
+            isActive={theme === 'light'}
+            >
+          </CircularButton>
+          <CircularButton
+            onClick={handleClick}
+            theme="rose"
+            color={roseThemePrimaryColor}
+            isActive={theme === 'rose'}
+            >
+          </CircularButton>
+          <CircularButton
+            onClick={handleClick}
+            theme="orange"
+            color={orangeThemePrimaryColor}
+            isActive={theme === 'orange'}
+            >
+          </CircularButton>
+          <CircularButton
+            onClick={handleClick}
+            theme="dark"
+            color={darkThemePrimaryColor}
+            isActive={theme === 'dark'}
+            >
+          </CircularButton>
+        </Box>
       </Box>
     </ThemeProvider>
   );
---
+++ src/components/CircularButton/CircularButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export default function CircleButton({ onClick, theme, color, isActive }) {
+
+  const handleClick = () => {
+    onClick(theme);
+  };
+
+  return (
+    <Button
+       onClick={handleClick}
+       size="small"
+       variant="contained" 
+       disableElevation
+       sx={{
+         minWidth: 0,
+         borderRadius: '50%',
+         height: '3em',
+         width: '3em',
+         backgroundColor: color,
+         backgroundColor: isActive ? 'primary.dark' : color,
+       }}>
+     </Button>
+  );
+}
+
